1edeeba511d361b7242521307ff30cbfe7a17ba1
Omar Yapur <omargit@gmail.com>
2026-05-24 11:25:04 -0600

    se arregla el if

--- ejemploRepositorio.cpp
+++ ejemploRepositorio.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main(){
     int a = 5, b = 7;
-    cout << "Hola mundo";
-    return 0;
+
+    cout << "Hola mundo" << endl;
+
     if (a < b){
+        cout << "a es menor que b";
+    } else {
         cout << "a es mayor que b";
     }
-    
+
+    return 0;
 }
